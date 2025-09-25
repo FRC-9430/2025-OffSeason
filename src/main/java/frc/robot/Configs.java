b289c0ff285da778e16e5ad121f4c1cb00400228
Brady Bontrager <bbontrager89@gmail.com>
2025-09-25 16:54:50 -0700

    I am making a message here and this is how I type in terminal using nano

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -1,8 +1,5 @@
 package frc.robot;
 
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import frc.robot.Constants.ModuleConstants;
 
