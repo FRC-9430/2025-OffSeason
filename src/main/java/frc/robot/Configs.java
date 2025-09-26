0457dce17dbcf55a14c4c5735c1cba13361ab530
Brady Bontrager <bbontrager89@gmail.com>
2025-09-25 17:02:41 -0700

    Revert "Merge branch 'Dev' of github.com:FRC-9430/2025-OffSeason into Dev"
    This reverts commit b9cd92547c65600ed61916dedd2bd1bda3c2d75c, reversing
    changes made to c2ed77b3474458c3859e540ce67ce05b90263974.

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -1,6 +1,4 @@
 package frc.robot;
 
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 
 import frc.robot.Constants.ModuleConstants;
