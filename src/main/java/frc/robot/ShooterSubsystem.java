8c3247cb49c4800b76573e6d760bcbb333605c36
Dabz26 <dylan.dablow@gmail.com>
2025-09-30 16:57:11 -0700

    Update ShooterSubsystem.java

--- src/main/java/frc/robot/ShooterSubsystem.java
+++ src/main/java/frc/robot/ShooterSubsystem.java
@@ -2,9 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.MotorConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
