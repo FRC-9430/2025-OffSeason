243851cd1b5a8799ee7c8fd1686e40b81c559580
Brady Bontrager <bbontrager89@gmail.com>
2025-09-30 17:54:52 -0700

    add two motor canID constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
 public final class Constants {
-
+    public static final class ShooterConstants {
+        public static final int kShooterPivotMotorCanId = 1;
+        public static final int kShooterIntakeMotorCanId = 2;
+    }
 }
