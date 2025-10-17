8c542498144022f15955341cd42de338ca8b9e22
IamIisI <titusj915@gmail.com>
2025-10-16 18:16:39 -0700

    Import Drive Subsystem Template and Add constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalSource;
+
+public class Constants {
+
+    public class DriveConstants {
+
+        public static final int kLeftMotor1Port = 0;
+        public static final int kLeftMotor2Port = 0;
+        public static final int kRightMotor1Port = 0;
+        public static final int kRightMotor2Port = 0;
+        public static final DigitalSource[] kLeftEncoderPorts = null;
+        public static final DigitalSource kLeftEncoderReversed = null;
+        public static final DigitalSource[] kRightEncoderPorts = null;
+        public static final DigitalSource kRightEncoderReversed = null;
+        public static final double kEncoderDistancePerPulse = 0;
+
+    }  
+}
