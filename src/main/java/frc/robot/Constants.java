f754e58c9f58ff1e78f289d8d882efc8d1cf1368
IamIisI <titusj915@gmail.com>
2025-10-30 12:49:55 -0700

    Add constants
    Most are just imported from previous code and requitre updating

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,7 +2,9 @@ package frc.robot;
 
 import com.revrobotics.spark.config.SparkBaseConfig;
 
-import edu.wpi.first.wpilibj.DigitalSource;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
 
 public class Constants {
 
@@ -19,9 +21,36 @@ public class Constants {
 
         public static final int pigeon2CanID = 0;
 
+        public static final double kMaxSpeedMetersPerSecond = 4.8;
+        public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+
+        // Chassis configuration
+        public static final double kTrackWidth = Units.inchesToMeters(22.5);
+
+        // Distance between centers of right and left wheels on robot
+        public static final double kWheelBase = Units.inchesToMeters(22.5);
+
+        // Distance between front and back wheels on robot
+        public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+                new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+        // Angular offsets of the modules relative to the chassis in radians
+        public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+        public static final double kFrontRightChassisAngularOffset = 0;
+        public static final double kBackLeftChassisAngularOffset = Math.PI;
+        public static final double kBackRightChassisAngularOffset = Math.PI / 2;
     }
 
-    
+    public class OIConstants {
+        public static final int kDriverControllerPort = 0;
+        public static final int kOperatorControllerPort = 1;
+        public static final double kDriveDeadband = 0.08;
+        public static final double kTriggerThreshold = 0.1;
+    }
+
     public static final class Configs {
 
         public static final class X2TSwerveModule {
@@ -29,7 +58,6 @@ public class Constants {
             public static final SparkBaseConfig turningConfig = null;
         };
 
-
     }
-        
+
 }
