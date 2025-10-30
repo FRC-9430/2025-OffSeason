7bbd6286229328987f2c09832598f5abaad1dd39
IamIisI <titusj915@gmail.com>
2025-10-30 12:50:31 -0700

    Import drive subsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -3,11 +3,37 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.Pigeon2;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.utils.SwerveModule;
 
 public class DriveSubsystem extends SubsystemBase {
+
+  // Each of these is one of our swerve modules (the wheels + motors that can spin
+  // and turn).
+  // We have four modules: front-left, front-right, back-left, and back-right.
+  public final SwerveModule m_frontLeft = new SwerveModule(
+      DriveConstants.kFrontLeftDriveMotorCanID,
+      DriveConstants.kFrontLeftTurningMotorCanID,
+      DriveConstants.kFrontLeftChassisAngularOffset);
+
+  public final SwerveModule m_frontRight = new SwerveModule(
+      DriveConstants.kFrontRightDriveMotorCanID,
+      DriveConstants.kFrontRightTurningMotorCanID,
+      DriveConstants.kFrontRightChassisAngularOffset);
+
+  public final SwerveModule m_rearLeft = new SwerveModule(
+      DriveConstants.kBackLeftDriveMotorCanID,
+      DriveConstants.kBackLeftTurningMotorCanID,
+      DriveConstants.kBackLeftChassisAngularOffset);
+
+  public final SwerveModule m_rearRight = new SwerveModule(
+      DriveConstants.kBackRightDriveMotorCanID,
+      DriveConstants.kBackRightTurningMotorCanID,
+      DriveConstants.kBackRightChassisAngularOffset);
 
   // The gyro sensor
   private final Pigeon2 m_gyro = new Pigeon2(DriveConstants.pigeon2CanID);
@@ -43,6 +69,25 @@ public class DriveSubsystem extends SubsystemBase {
    */
   public void resetOdometry(Pose2d pose) {
     
+  }
+
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+    // Convert the commanded speeds into the correct units for the drivetrain
+    double xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond;
+    double ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond;
+    double rotDelivered = rot * DriveConstants.kMaxAngularSpeed;
+    // TODO: FIX TO USE TO ROBOT RELATIVE SPEEDS
+    var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
+        fieldRelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered,
+                m_gyro.getRotation2d())
+            : new ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered));
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(swerveModuleStates[0]);
+    m_frontRight.setDesiredState(swerveModuleStates[1]);
+    m_rearLeft.setDesiredState(swerveModuleStates[2]);
+    m_rearRight.setDesiredState(swerveModuleStates[3]);
   }
 
 
