3a801300c2a8238fef35e349d8d6547f6a3a946c
FRCteam9430 <frc.9430@outlook.com>
2025-10-30 18:22:42 -0700

    Import Config static

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.geometry.Translation2d;
@@ -72,8 +74,70 @@ public class Constants {
         public static final class X2TSwerveModule {
             public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
             public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
+            
+            static {
+                // Use module constants to calculate conversion factors and feed forward gain.
+                double drivingFactor = ModuleConstants.kWheelDiameterMeters * Math.PI
+                        / ModuleConstants.kDrivingMotorReduction;
+                double turningFactor = 2 * Math.PI;
+                double drivingVelocityFeedForward = 1 / ModuleConstants.kDriveWheelFreeSpeedRps;
+    
+                drivingConfig
+                        .idleMode(IdleMode.kBrake)
+                        .smartCurrentLimit(50);
+                drivingConfig.encoder
+                        .positionConversionFactor(drivingFactor) // meters
+                        .velocityConversionFactor(drivingFactor / 60.0); // meters per second
+                drivingConfig.closedLoop
+                        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                        // These are example gains you may need to them for your own robot!
+                        .pid(0.04, 0, 0)
+                        .velocityFF(drivingVelocityFeedForward)
+                        .outputRange(-1, 1);
+    
+                turningConfig
+                        .idleMode(IdleMode.kBrake)
+                        .smartCurrentLimit(20);
+                turningConfig.absoluteEncoder
+                        // Invert the turning encoder, since the output shaft rotates in the opposite
+                        // direction of the steering motor in the MAXSwerve Module.
+                        .inverted(true)
+                        .positionConversionFactor(turningFactor) // radians
+                        .velocityConversionFactor(turningFactor / 60.0); // radians per second
+                turningConfig.closedLoop
+                        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+                        // These are example gains you may need to them for your own robot!
+                        .pid(1, 0, 0)
+                        .outputRange(-1, 1)
+                        // Enable PID wrap around for the turning motor. This will allow the PID
+                        // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
+                        // to 10 degrees will go through 0 rather than the other direction which is a
+                        // longer route.
+                        .positionWrappingEnabled(true)
+                        .positionWrappingInputRange(0, turningFactor);
+            }
         };
 
+        
+
+        public static final class ModuleConstants {
+            // The MAXSwerve module can be configured with one of three pinion gears: 12T,
+            // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+            // more teeth will result in a robot that drives faster).
+            public static final int kDrivingMotorPinionTeeth = 14;
+        
+            // Calculations required for driving motor conversion factors and feed forward
+            public static final double kDrivingMotorFreeSpeedRps = 5676 / 60;
+            public static final double kWheelDiameterMeters = 0.0729;
+            public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+            // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+            // teeth on the bevel pinion
+            // TODO bbontrager89 20241107.1742: Need to update values for
+            // kDrivingMotorReduction
+            public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
+            public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+                / kDrivingMotorReduction;
+          }
     }
 
 }
