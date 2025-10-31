5d99511e4c700dfaafe8c0e46ce361dfaab52041
IamIisI <titusj915@gmail.com>
2025-10-30 17:51:48 -0700

    Update robot container to incorperate more of previos code functions

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,13 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -13,40 +18,48 @@ import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
-    // The robot's subsystems
-    private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+        // The robot's subsystems
+        private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
-    // The driver's controller
-    public static CommandXboxController c_driverController = new CommandXboxController(
-            OIConstants.kDriverControllerPort);
+        // The driver's controller
+        public static CommandXboxController c_driverController = new CommandXboxController(
+                        OIConstants.kDriverControllerPort);
 
-    // The operator's controller
-    public static CommandXboxController c_operatorController = new CommandXboxController(
-            OIConstants.kOperatorControllerPort);
+        // The operator's controller
+        public static CommandXboxController c_operatorController = new CommandXboxController(
+                        OIConstants.kOperatorControllerPort);
 
-    public RobotContainer() {
-        configureBindings();
+        private final SendableChooser<Command> autoChooser;
 
-        // Configure default commands
-        m_robotDrive.setDefaultCommand(
-                // The left stick controls translation of the robot.
-                // Turning is controlled by the X axis of the right stick.
-                new RunCommand(
-                        () -> m_robotDrive.drive(
-                                -MathUtil.applyDeadband(c_driverController.getLeftY(),
-                                        OIConstants.kDriveDeadband),
-                                -MathUtil.applyDeadband(c_driverController.getLeftX(),
-                                        OIConstants.kDriveDeadband),
-                                -MathUtil.applyDeadband(c_driverController.getRightX(),
-                                        OIConstants.kDriveDeadband),
-                                true),
-                        m_robotDrive));
-    }
+        public RobotContainer() {
+                configureBindings();
 
-    private void configureBindings() {
-    }
+                autoChooser = AutoBuilder.buildAutoChooser();
 
-    public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
-    }
+                SmartDashboard.putData("Auto Chooser", autoChooser);
+
+                // Configure default commands
+                m_robotDrive.setDefaultCommand(
+                                // The left stick controls translation of the robot.
+                                // Turning is controlled by the X axis of the right stick.
+                                new RunCommand(
+                                                () -> m_robotDrive.drive(
+                                                                -MathUtil.applyDeadband(c_driverController.getLeftY(),
+                                                                                OIConstants.kDriveDeadband),
+                                                                -MathUtil.applyDeadband(c_driverController.getLeftX(),
+                                                                                OIConstants.kDriveDeadband),
+                                                                -MathUtil.applyDeadband(c_driverController.getRightX(),
+                                                                                OIConstants.kDriveDeadband),
+                                                                true),
+                                                m_robotDrive));
+
+                CommandScheduler.getInstance().run();
+        }
+
+        private void configureBindings() {
+        }
+
+        public Command getAutonomousCommand() {
+                return autoChooser.getSelected();
+        }
 }
