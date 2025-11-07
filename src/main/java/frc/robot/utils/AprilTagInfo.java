87070af99eeed035bce6fda68c402272f4c9a6da
IamIisI <titusj915@gmail.com>
2025-11-06 18:01:18 -0800

    Create class for getting april tag information

---
+++ src/main/java/frc/robot/utils/AprilTagInfo.java
@@ -0,0 +1,11 @@
+package frc.robot.utils;
+
+import edu.wpi.first.math.geometry.Translation3d;
+
+public class AprilTagInfo {
+
+
+    public static Translation3d GetTranslation3d(int id) {
+        return new Translation3d();
+    }
+}
