665091e4e31ee4f2fc5517e1e142abb0dea294a6
IamIisI <titusj915@gmail.com>
2025-11-06 17:22:18 -0800

    Remove unused imports

--- src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
@@ -6,15 +6,12 @@ package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.DriveConstants;
 
 /**
  * Pose estimation subsyestem designed to overwrite the default swerve odometry
