ae9eac55bd4e4de8959ec8868f6d27ecdc3b026e
PeaceCatis <littlemissamayapie@gmail.com>
2025-12-02 16:41:29 -0800

    Created files for commands

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,27 +6,33 @@ package frc.robot;
 
 /** Add your docs here. */
 public final class Constants {
-    public static final class OIConstants{
+    public static final class OIConstants {
         public static final int k_operatorControllerPort = 1;
     }
-    public static final class MotorConstants{
+
+    public static final class MotorConstants {
         public static final int IntakeMotorCanID = 41;
         public static final int ShooterMotorCanID = 42;
         public static final int ElevatorMotorCanID = 27;
-        public static final double kElevatorMaxHeight = 0; //TODO set value    
-        public static final double kElevatorMinHeight = 0; //TODO set value
+        public static final double kElevatorMaxHeight = 0.975;
+        public static final double kElevatorMinHeight = 0.160;
         public static final double kShooterPivotMaxHeight = .48;
         public static final double kShooterPivotMinHeight = .090;
 
     }
-    public static final class PIDConstants{
-        public static final double kShooterkp = 0.1; //TODO: set shooter PID values
+
+    public static final class PIDConstants {
+        public static final double kShooterkp = 0.1; 
         public static final double kShooterki = 0;
         public static final double kShooterkd = 0;
-        public static final double kElevatorkp = 0.1; //TODO: set elevator PID values
+        public static final double kElevatorkp = 20; 
         public static final double kElevatorki = 0;
-        public static final double kElevatorkd = 0;
-        public static final double kIntakeSetpoint = .2;
-        public static final double kScoreSetpoint = .1;
+        public static final double kElevatorkd = 0.5;
+        public static final double kShooterTransitSetpoint = .255;
+        public static final double kShooterIntakeSetpoint = .2;
+        public static final double kShooterScoreSetpoint = .1;
+        public static final double kElevatorTransitSetpoint = .255;
+        public static final double kElevatorIntakeSetpoint = .2;
+        public static final double kElevatorScoreSetpoint = .1;
     }
 }
