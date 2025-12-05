9ebc51132089a2166124134d84a46ab4c8c0a180
PeaceCatis <littlemissamayapie@gmail.com>
2025-12-05 13:51:02 -0800

    Adding basic set up for commands

--- src/main/java/frc/robot/Command/intakepostion.java
+++ src/main/java/frc/robot/Command/intakepostion.java
@@ -1,5 +1,18 @@
 package frc.robot.Command;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+
+
 
 public class intakepostion {
-    
+    ShooterSubsystem coralSubsystem;
+    double speed;
+    double timeoutDuration;
+
+
+
+
+
 }
--- src/main/java/frc/robot/Command/transitpostion.java
+++ src/main/java/frc/robot/Command/transitpostion.java
@@ -1,5 +1,12 @@
 package frc.robot.Command;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
 
 public class transitpostion {
+    ShooterSubsystem coralSubsystem;
+    double speed;
+    double timeoutDuration;
     
 }
