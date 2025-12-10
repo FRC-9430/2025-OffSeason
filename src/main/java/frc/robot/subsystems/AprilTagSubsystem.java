407feced01d5bba905a8399c1e1fd8ef7091df31
maddie 3 <maddiedvillasenor@gmail.com>
2025-12-09 17:27:52 -0800

    Completed Basic Automation

--- src/main/java/frc/robot/subsystems/AprilTagSubsystem.java
+++ src/main/java/frc/robot/subsystems/AprilTagSubsystem.java
@@ -35,10 +35,12 @@ public class AprilTagSubsystem{
         //getting pipeline result, checking for targets
         PhotonPipelineResult targets = camera.getLatestResult();
         boolean presence = targets.hasTargets();
-    
+
+        //verifying and executing printing function
           if (presence == true){
             printLatestResult();
           }
+        
          
 
 
