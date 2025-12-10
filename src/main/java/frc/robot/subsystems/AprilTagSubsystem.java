da82b1f7ce81a109a77832192ceab60c8ebe0783
maddie 3 <maddiedvillasenor@gmail.com>
2025-12-09 17:38:00 -0800

    Revert "Merge branch '4-apriltags' of https://github.com/FRC-9430/2025-OffSeason into 4-apriltags"
    This reverts commit 34164a9d1f00927edb3871c542cbf0455e7175db, reversing
    changes made to 702fbe0b7ec06291f91ed44a7f65126d08c80967.

--- src/main/java/frc/robot/subsystems/AprilTagSubsystem.java
+++ src/main/java/frc/robot/subsystems/AprilTagSubsystem.java
@@ -36,18 +36,6 @@ public class AprilTagSubsystem{
         System.out.println();
     }
 
-    public void printTargetInformation() {
-        //getting pipeline result, checking for targets
-        PhotonPipelineResult targets = camera.getLatestResult();
-        boolean presence = targets.hasTargets();
-
-        //verifying and executing printing function
-          if (presence == true){
-            printLatestResult();
-          }
-        
-         
 
 
-    }
 }
