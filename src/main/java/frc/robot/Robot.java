c68845699e640320533826f107ecab2d204dff75
FRCteam9430 <frc.9430@outlook.com>
2025-12-23 16:33:42 -0800

    Use Pathfinder ADStarAK
    -Titus

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.pathfinding.Pathfinding;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.utils.LocalADStarAK;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -14,6 +17,7 @@ public class Robot extends TimedRobot {
     private final RobotContainer m_robotContainer;
 
     public Robot() {
+        Pathfinding.setPathfinder(new LocalADStarAK());
         m_robotContainer = new RobotContainer();
     }
 
